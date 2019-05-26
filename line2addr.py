7aff474f44e4795936e5ed8e94c4a004a07ebc6b
Chris Greene <archgoon@gmail.com>
2019-05-25 18:26:23 -0700

    [refact] Move dwarf logic to func

--- line2addr.py
+++ line2addr.py
@@ -4,6 +4,19 @@ from collections import defaultdict as dd
 
 import elftools.elf.elffile as elf
 
+def get_lines(binary):
+    belf = elf.ELFFile(binary)
+    dwarf = belf.get_dwarf_info()
+    lines = dd(lambda: dd(lambda:[]))
+    for cu in dwarf.iter_CUs():
+        lp = dwarf.line_program_for_CU(cu)
+        files = lp['file_entry']
+        for lpe in lp.get_entries():
+            if lpe.state:
+                lfile = files[lpe.state.file-1]
+                lines [(lfile['dir_index'], lfile['name'])][lpe.state.line].append(lpe.state.address)
+    return lines
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", "-f")
@@ -11,15 +24,6 @@ def main():
     options = parser.parse_args()
 
     with open(options.binary, "rb") as binary:
-        belf = elf.ELFFile(binary)
-        dwarf = belf.get_dwarf_info()
-        lines = dd(lambda: dd(lambda:[]))
-        for cu in dwarf.iter_CUs():
-            lp = dwarf.line_program_for_CU(cu)
-            files = lp['file_entry']
-            for lpe in lp.get_entries():
-                if lpe.state:
-                    lfile = files[lpe.state.file-1]
-                    lines [(lfile['dir_index'], lfile['name'])][lpe.state.line].append(lpe.state.address)
+        lines = get_lines(binary)
     print(lines)
 main()
