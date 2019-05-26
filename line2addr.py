970e97ca02bdd642529954d29b0560a4356ba662
Chris Greene <archgoon@gmail.com>
2019-05-25 19:00:24 -0700

    Given a file, display the addresses

--- line2addr.py
+++ line2addr.py
@@ -1,8 +1,16 @@
 import argparse
+import colorama
+import os
 
 from collections import defaultdict as dd
 
 import elftools.elf.elffile as elf
+
+def redhex(num, padding):
+    return colorama.Fore.RED + ("{:"+str(padding)+"x}").format(num)+ colorama.Fore.RESET
+
+def yellownum(num, padding):
+    return colorama.Fore.YELLOW + ("{:" + str(padding) + "d}").format(num) + colorama.Fore.RESET
 
 def get_lines(binary):
     belf = elf.ELFFile(binary)
@@ -17,6 +25,25 @@ def get_lines(binary):
                 lines [(lfile['dir_index'], lfile['name'])][lpe.state.line].append(lpe.state.address)
     return lines
 
+def display_file(filename, lines):
+    referenced_files = {pair[1]:(pair[0],pair[1]) for pair in lines}
+    bf = bytes(os.path.basename(filename), 'utf8')
+
+    with open(filename) as srcfile:
+        reffile = referenced_files.get(bf, None)
+        if reffile:
+            for lineno, line in enumerate(srcfile.readlines(), 1):
+                if lineno in lines[reffile]:
+                    addresses = lines[reffile][lineno]
+                    print("{} {} {}".format(yellownum(lineno, 3), redhex(addresses[0], 8), line[:-1]))
+                    for i, addr in enumerate(addresses[1:], 1):
+                        print("{:3} {}".format("", redhex(addr, 8)))
+                else:
+                    print("{} {:8} {}".format(yellownum(lineno, 3), '', line[:-1]))
+        else:
+            print("{} is not references in the executable".format(filename))
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", "-f")
@@ -25,5 +52,6 @@ def main():
 
     with open(options.binary, "rb") as binary:
         lines = get_lines(binary)
-    print(lines)
+    if options.file:
+        display_file(options.file, lines)
 main()
